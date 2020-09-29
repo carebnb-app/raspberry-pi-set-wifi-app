509b2b24e5d02d5e36909cbcc726702ac095527b
deguilardi <demetrio.guilardi@gmail.com>
2020-09-29 19:48:01 -0400

    normalizing react names

--- react-native/android/app/src/main/java/app/carebnb/raspberrywifi/MainApplication.java
+++ react-native/android/app/src/main/java/app/carebnb/raspberrywifi/MainApplication.java
@@ -8,7 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.wificonnect.BuildConfig;
+import app.carebnb.raspberrywifi.BuildConfig;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
