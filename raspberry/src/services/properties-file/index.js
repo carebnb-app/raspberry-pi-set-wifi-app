b5809baf8095360012c34bdf4e8d171bc76f1e20
deguilardi <demetrio.guilardi@gmail.com>
2020-10-14 12:13:21 -0400

    A little more verbosity at saving and loading properties file

--- raspberry/src/services/properties-file/index.js
+++ raspberry/src/services/properties-file/index.js
@@ -3,14 +3,21 @@ import fs from 'fs'
 
 export const write = (properties) => {
   const fileContent = JSON.stringify(properties)
-  fs.writeFile(config.CUSTOM_PROPERTIES_FILE, fileContent)
+  console.log(`Saving properties on file with: ${fileContent}`)
+  fs.writeFile(config.CUSTOM_PROPERTIES_FILE, fileContent, (err, result) => {
+    if(err){
+      console.error('Error saving properties file')
+    }
+  })
 }
 
 export const read = () => {
   try{
     const fileContent = fs.readFileSync(config.CUSTOM_PROPERTIES_FILE)
+    console.log(`Loaded properties from file with: ${fileContent}`)
     return JSON.parse(fileContent)
   } catch {
+    console.error('Error loading properties file')
     return {}
   }
 }
