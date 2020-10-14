641c717b415a5df1c56dce301428b8fca2e2827e
deguilardi <demetrio.guilardi@gmail.com>
2020-10-14 17:05:25 +0100

    fix: removing async word

--- raspberry/src/services/properties-file/index.js
+++ raspberry/src/services/properties-file/index.js
@@ -1,7 +1,7 @@
 import * as config from '../../../config'
 import fs from 'fs'
 
-export async const write = (properties) => {
+export const write = (properties) => {
   const fileContent = JSON.stringify(properties)
   fs.writeFile(config.CUSTOM_PROPERTIES_FILE, fileContent)
 }
